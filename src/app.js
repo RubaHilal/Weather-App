eeaf91075e99d389c4e920cb5aedf823c5fd997e
Ruba Hilal <123831515+RubaHilal@users.noreply.github.com>
2023-05-03 15:12:29 +0200

    JS wind, city, temp, humidity with style

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+	console.log(response.data.main.temp);
+	let currentTemperature = document.querySelector("#temperature");
+	let currentCity = document.querySelector("#city");
+	let currentHumidity = document.querySelector("#humidity");
+	let windSpeed = document.querySelector("#wind");
+	currentTemperature.innerHTML = Math.round(response.data.main.temp);
+	currentCity.innerHTML = response.data.name;
+	currentHumidity.innerHTML = response.data.main.humidity;
+	windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "0df4d3e847b64a8832063c084ffc9e7f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= San Francisco &appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
