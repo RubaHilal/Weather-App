b21c6afffe77d212eff73f56c78aeb0e9555a844
Ruba Hilal <123831515+RubaHilal@users.noreply.github.com>
2023-05-03 15:27:27 +0200

    JS description

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ function displayTemperature(response) {
 	console.log(response.data.main.temp);
 	let currentTemperature = document.querySelector("#temperature");
 	let currentCity = document.querySelector("#city");
+	let currentDescription = document.querySelector("#description");
 	let currentHumidity = document.querySelector("#humidity");
 	let windSpeed = document.querySelector("#wind");
 	currentTemperature.innerHTML = Math.round(response.data.main.temp);
 	currentCity.innerHTML = response.data.name;
 	currentHumidity.innerHTML = response.data.main.humidity;
 	windSpeed.innerHTML = Math.round(response.data.wind.speed);
+	currentDescription.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "0df4d3e847b64a8832063c084ffc9e7f";
