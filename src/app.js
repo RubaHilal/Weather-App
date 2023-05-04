10404db6b636d52fcb6bc8f3c08e76c202b04dea
Ruba Hilal <123831515+RubaHilal@users.noreply.github.com>
2023-05-04 12:07:23 +0200

    JS icon set up

--- src/app.js
+++ src/app.js
@@ -27,15 +27,22 @@ function displayTemperature(response) {
 	let currentHumidity = document.querySelector("#humidity");
 	let windSpeed = document.querySelector("#wind");
 	let currentDate = document.querySelector("#date");
+	let iconElement = document.querySelector("#icon");
 	currentTemperature.innerHTML = Math.round(response.data.main.temp);
 	currentCity.innerHTML = response.data.name;
 	currentHumidity.innerHTML = response.data.main.humidity;
 	windSpeed.innerHTML = Math.round(response.data.wind.speed);
 	currentDescription.innerHTML = response.data.weather[0].description;
 	currentDate.innerHTML = formatDate(response.data.dt * 1000);
+	iconElement.setAttribute(
+		"src",
+		`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png `
+	);
+	iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "0df4d3e847b64a8832063c084ffc9e7f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= San Francisco &appid=${apiKey}&units=metric`;
+let city = "San Francisco";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= ${city} &appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
